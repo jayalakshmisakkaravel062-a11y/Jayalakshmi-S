88191a213ffe45bc3f86bf5f8afab5e009ee02a0
jayalakshmisakkaravel062-a11y <jayalakshmisakkaravel062@gmail.com>
2026-02-19 10:24:39 +0530

    Add files via upload

---
+++ Rainfall pattern analyzer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n, rain, total = 0, heavyDays = 0;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) {
+        scanf("%d", &rain);
+        total = total + rain;
+        if(rain > 50) {
+            heavyDays++;
+        }
+    }
+    printf("Total Rainfall: %d\n", total);
+    printf("Heavy Rain Days: %d", heavyDays);
+    return 0;
+}
