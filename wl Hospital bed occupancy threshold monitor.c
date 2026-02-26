626e9f1bbeb54509940939a6cb8fe6424b5e76f9
jayalakshmisakkaravel062-a11y <jayalakshmisakkaravel062@gmail.com>
2026-02-26 15:38:34 +0530

    Add files via upload

---
+++ wl Hospital bed occupancy threshold monitor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int capacity, N;
+    scanf("%d%d", &capacity, &N);
+    int change, occupied = 0, critical = 0, i = 0;
+    while(i < N) {
+        scanf("%d", &change);
+        occupied += change;
+        if(occupied > 0.9 * capacity)
+            critical++;
+        i++;
+    }
+    printf("Final Occupied Beds: %d\n", occupied);
+    printf("Critical Hours: %d\n", critical);
+    return 0;
+}
