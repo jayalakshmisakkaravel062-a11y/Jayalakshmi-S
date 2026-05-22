1fbcc7c3730a58bea3be8823b6f67254bdf4ab64
jayalakshmisakkaravel062-a11y <jayalakshmisakkaravel062@gmail.com>
2026-05-22 11:44:32 -0400

    Add files via upload

---
+++ arr check asending order.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]>a[j]){
+                   System.out.println("no");
+                   return;
+                }
+                else{
+                    System.out.println("yes");
+                    return;
+                }
+            }
+        }
+    }
+}
