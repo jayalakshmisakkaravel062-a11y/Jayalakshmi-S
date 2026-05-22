f15fc3e4a6fd54e5156040c369183e35cb29d0d4
jayalakshmisakkaravel062-a11y <jayalakshmisakkaravel062@gmail.com>
2026-05-22 02:45:17 -0400

    Add files via upload

---
+++ arr swap last half.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        for(int i=n/2,j=n-1;i<j;i++,j--){
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+        }
+        for(int i=0;i<n;i++){
+             System.out.print(a[i]+" ");
+        }
+    }
+}
