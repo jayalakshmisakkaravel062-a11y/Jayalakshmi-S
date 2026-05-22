b8bb3e47384a8fe45c6d6e5f1028ef4967b57f23
jayalakshmisakkaravel062-a11y <jayalakshmisakkaravel062@gmail.com>
2026-05-21 21:48:56 -0400

    Add files via upload

---
+++ 2arr oddsum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int [n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        arr(a,n);
+    
+    }
+    public static void arr(int b[],int m){
+        int sum=0;
+        for(int i=0;i<m;i++){
+            if(b[i]%2==0){
+                sum=sum+b[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
