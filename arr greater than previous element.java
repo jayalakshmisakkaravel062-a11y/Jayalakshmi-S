9a214467ddc50995941171a8a49dde90e16c052b
jayalakshmisakkaravel062-a11y <jayalakshmisakkaravel062@gmail.com>
2026-05-25 11:59:08 -0400

    Add files via upload

---
+++ arr greater than previous element.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        int max=a[0];
+        System.out.print(a[0]+" ");
+        for(int i=0;i<n;i++){
+            if(a[i]>max){
+                System.out.print(a[i]+" ");
+                max=a[i];
+            }
+        }
+    }
+}
