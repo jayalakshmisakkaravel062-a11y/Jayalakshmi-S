fb5991e1b19eb2352dd30a689c86528808649513
jayalakshmisakkaravel062-a11y <jayalakshmisakkaravel062@gmail.com>
2026-06-02 11:26:22 -0400

    Add files via upload

---
+++ 2d sum of each row.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int r=s.nextInt(),c=s.nextInt();
+        int a[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                a[i][j]=s.nextInt();
+            }                                                    
+        }
+        int sum=0;
+        for(int i=0;i<r;i++){
+             for(int j=0;j<c;j++){
+                 sum=sum+a[i][j];
+            }
+            System.out.print(sum+" ");
+            sum=0;
+        }
+    }
+}
